8d29eee144500ff8833fc5bc597ff0c920ca3378
Md Ashik <mashikahamed0@gmail.com>
2024-10-14 03:25:07 +0600

    Delete res directory

--- res/scripts/script.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector('.header');
-const welcome = document.querySelector('.welcome');
-
-let lastScrollY = 0;
-let isHidden = false;
-
-window.addEventListener('scroll', () => {
-    const currentScrollY = window.scrollY
