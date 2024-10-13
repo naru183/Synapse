478948f63cb6c3ccca1811bfa4f34fcb187471e4
Md Ashik <mashikahamed0@gmail.com>
2024-10-13 22:19:50 +0600

    Add files via upload

--- layouts/NotFound.js
+++ layouts/NotFound.js
@@ -1,11 +1,12 @@
 // import node module libraries
 import { Container } from 'react-bootstrap';
 
-const AuthLayout = (props) => {
+const NotFound = (props) => {
 	return (
-		<Container className="d-flex flex-column">
+		<Container className="d-flex flex-column" >
 			{props.children}
 		</Container>
 	);
 };
-export default AuthLayout;
+
+export default NotFound;
